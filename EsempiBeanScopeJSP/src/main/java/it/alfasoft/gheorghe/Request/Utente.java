c6f6b7d3946dd321f7342b428d42ea90fc685849
bahovgheorghe <giorgiobahov@gmail.com>
2016-11-09 09:48:01 +0200

    Aggiunto la gestione dei campi Cellulare e Codice Fiscale

--- EsempiBeanScopeJSP/src/main/java/it/alfasoft/gheorghe/Request/Utente.java
+++ EsempiBeanScopeJSP/src/main/java/it/alfasoft/gheorghe/Request/Utente.java
@@ -1,7 +1,12 @@
 package it.alfasoft.gheorghe.Request;
 import java.io.Serializable;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+import javax.xml.bind.ValidationException;
 @ManagedBean(name="utente",eager=true)
 public class Utente implements Serializable{
 	
@@ -13,7 +18,8 @@ public class Utente implements Serializable{
 	private String username;
 	private String password;
 	private int eta;
-	
+	private String telefono;
+	private String codFiscale;
 	public Utente(){
 		
 	}
@@ -41,7 +47,39 @@ public class Utente implements Serializable{
 	public void setEta(int eta) {
 		this.eta = eta;
 	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getCodFiscale() {
+		return codFiscale;
+	}
+
+	public void setCodFiscale(String codFiscale) {
+		this.codFiscale = codFiscale;
+	}
 	
-	
+	public void validaCodFisc(FacesContext comntext, 
+							  UIComponent component,
+							  Object value) throws ValidatorException{
+
+		if (value==null){
+			return;
+		}
+		
+		String dato = value.toString();
+		
+		if(!dato.startsWith("aaa")){
+			FacesMessage msg = new FacesMessage("Codice Fiscale Errato!");
+			throw new ValidatorException(msg);
+		}
+		
+		
+	}
 
 }
